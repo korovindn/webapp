228ea5687e484f7b97d98ed7d523659298448d1b
Коровин Дмитрий Николаевич <DNKorovin@sberbank.ru>
2024-02-20 05:08:20 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <Router basename="/actions_test">
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
